0e74f1abfef81b542ebacdfcb30faa19e56c7d9c
Prakhar Agarwal <prakhar@everything.net.in>
2021-12-31 13:15:56 +0530

    Update Buy and Sell 1.java

--- Buy and Sell 1.java
+++ Buy and Sell 1.java
@@ -1,7 +1,7 @@
 class Solution {
   public int maxProfit(int[] prices) {
-    int buy = prices[0], profit = 0;
-    for (int i = 1; i < prices.length; i++) {
+    int buy = Integer.MAX_VALUE, profit = 0;
+    for (int i = 0; i < prices.length; i++) {
       buy = Math.min(buy, prices[i]);
       profit = Math.max(profit, prices[i] - buy);
     }
