c300358fbdbf95aab2524bf23f247dd1fc85709c
Prakhar Agarwal <prakhar@everything.net.in>
2022-01-01 23:14:18 +0530

    Update Buy and Sell 2.java

--- Buy and Sell 2.java
+++ Buy and Sell 2.java
@@ -1,3 +1,20 @@
+// Method 1
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int maxProfit(final int[] A) {
+        if (A.length == 0) return 0;
+        int profit = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > A[i-1]) {
+                profit += A[i]-A[i-1];
+            }
+        }
+        return profit;
+    }
+}
+
+// Method 2
+
 class Solution {
   public int maxProfit(int[] prices) {
     int buy = prices[0], profit = 0;
